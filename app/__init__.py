362ac5ebb042e741895f46ddc0f19d1e2f050ff8
lebovskii <lebovskiil@gmail.com>
2024-12-02 21:45:04 -0800

    fixed env, conected to db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .auth import auth
 from config import TEMPLATES_PATH
+from logger import logger
 
 app = Flask(__name__, template_folder= TEMPLATES_PATH)
 app.register_blueprint(auth, url_prefix= '/auth')
+logger.debug('The app started')
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import create_engine
 from config import DB_PASSWORD,DB_PORT,DB_USER,DB_HOST,DB_NAME
-
+from logger import logger
 
 class Base(DeclarativeBase):
     pass
 
 
 engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
 Base.metadata.create_all(engine)
+logger.debug('db initialized')
